6e9af01bfa545e59ed7e75993ee18afc2e4992e2
Lyssie Wright <lyssiewright.15@gmail.com>
2022-01-19 17:19:19 -0600

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+const messages = [];
+function writeCards(names, event){
+    const messages = [];
+    for (let i = 0; i < names.length; i++){
+       messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return (messages);
+}
+
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+
+function countDown(n){
+    while (n >= 0){
+        console.log(n--);
+    }
+}
+
+countDown(10);
